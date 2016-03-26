5007cd58ef0400b85bdeac5127eddbe140d3aa83
Thomas Nappo <technology@live.com>
2016-03-26 18:48:45 -0400

    Made generations based off indent

--- src/main/kotlin/org/jire/pomade/pom/Plugin.kt
+++ src/main/kotlin/org/jire/pomade/pom/Plugin.kt
@@ -3,8 +3,8 @@ package org.jire.pomade.pom
 class Plugin(val artifact: Artifact, val executions: Set<Execution>) : XMLElement {
 
 	override fun generate(indent: Int): String {
-		var string = "${indent()}<plugin>\n${artifact.generate(2)}\n${indent(1)}<executions>\n"
-		executions.forEach { string += it.generate(3) }
+		var string = "${indent()}<plugin>\n${artifact.generate(indent + 2)}\n${indent(1)}<executions>\n"
+		executions.forEach { string += it.generate(indent + 3) }
 		return string + "${indent(1)}</executions>\n${indent()}</plugin>"
 	}
 
